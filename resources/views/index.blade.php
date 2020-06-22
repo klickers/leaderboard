5cd7a61b1e518671bc5bb33e5a3c52e2448eec61
natborks <natborksdeutsch@yahoo.com>
2020-06-22 23:29:38 +0000

    change UI

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -9,78 +9,47 @@ $count = 0;
 $rowclass = 'grey';
 @endphp
 
-<div class="limiter">
-    <div class="container-table100">
-        <div class="wrap-table100">
-                <div class="table">
+<div class="wrapper">
 
-                    <div class="row header">
-                        <div class="cell">
-                            Full Name
-                        </div>
-                        <div class="cell">
-                            Username
-                        </div>
-                        <div class="cell">
-                            Email
-                        </div>
-                        <div class="cell">
-                            Points
-                        </div>
-                    </div>
-                    @foreach($array as $arra)
-                        @foreach($arra as $arr)
-                        @if($count <= 2)
-                            <div class="row" style="background-color:#ffcccc">
-                        @else
-                            <div class="row">
-                        @endif
-                                <div class="cell" data-title="Full Name">
-                                    <p>{{ $arr["full_name"] }}</p>
-                                </div>
-                                <div class="cell" data-title="Age">
-                                    <p>{{ $arr["username"] }}</p>
-                                </div>
-                                <div class="cell" data-title="Job Title">
-                                    <p>{{ $arr["email"] }}</p>
-                                </div>
-                                <div class="cell" data-title="Location">
-                                    <p>{{ $arr["total_points"] }}</p>
-                                </div>
-                                @php
-                                $count = $count + 1
-                                @endphp
+    <div class="container">
+      <div class="row">
+        <div class="col-md-8 col-md-offset-2">
+          <div class="description">
+          </div>
 
-                        </div>
-                        @endforeach
+          <div class="fresh-table  toolbar-color-azure">
+          <!--
+            Available colors for the full background: full-color-blue, full-color-azure, full-color-green, full-color-red, full-color-orange
+            Available colors only for the toolbar: toolbar-color-blue, toolbar-color-azure, toolbar-color-green, toolbar-color-red, toolbar-color-orange
+          -->
+
+            <table id="fresh-table" class="table">
+              <thead>
+                <th data-field="full_name" data-sortable="true">Name</th>
+                <th data-field="username" data-sortable="true">username</th>
+                <th data-field="email" data-sortable="true">Email</th>
+                <th data-field="total_points" data-sortable="true">Total Points</th>
+                <th data-field="actions" data-formatter="operateFormatter" data-events="operateEvents">Actions</th>
+              </thead>
+              <tbody>
+                @foreach($array as $arra)
+                @foreach($arra as $arr)
+                    <tr>
+                    <td>{{ $arr["full_name"] }}</td>
+                    <td>{{ $arr["username"] }}</td>
+                    <td>{{ $arr["email"] }}</td>
+                    <td>{{ $arr["total_points"] }}</td>
+                    <td></td>
+                    </tr>
                     @endforeach
-                </div>
+                    @endforeach
+              </tbody>
+            </table>
+          </div>
 
+            {{-- <p>Copyright &copy; 2019 <a href="http://creative-tim.com">Creative Tim</a>, made with <i class="fa fa-heart ct-heart"></i> for a better web.</p> --}}
+          </div>
         </div>
+      </div>
     </div>
-</div>
-<div class="sharethis-inline-share-buttons"></div>
-
-@endsection
-
-
-{{-- @foreach($array as $arra)
-<div>
-    @foreach($arra as $arr)
-        <div class="row">
-            <div class="cell" data-title="Full Name">
-                <p>{{ $arr[0] }}</p>
-            </div>
-            <div class="cell" data-title="Username">
-                <p>{{ $arr[1] }}</p>
-            </div>
-            <div class="cell" data-title="Email">
-                <p>{{ $arr[2] }}</p>
-            </div>
-            <div class="cell" data-title="Total Points">
-                <p>{{ $arr[3] }}</p>
-            </div>
-        </div>
-    @endforeach
-</div>
-@endforeach --}}
+  </div>
